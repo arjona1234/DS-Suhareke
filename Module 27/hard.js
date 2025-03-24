00df0a3a81f848d8c4372498195cc1a7c9523af4
arjona <arjonaelezi@gmail.com>
2025-03-24 19:49:05 +0100

    .

--- Module 27/hard.js
+++ Module 27/hard.js
@@ -11,12 +11,15 @@ function Book(title, author, pages) {
 const myBook = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 218);
 myBook.printSummary();  
 
-};
-function Movie(title, director, duration){ 
-    this.title=title;
-    this.director=director;
-    this.director=director;
+function Movie(title, director, duration) { 
+    this.title = title;
+    this.director = director;
+    this.duration = duration;
+
     this.printSummary = function() {
-        console.log('Inspection', '')
-    }
+        console.log(`${this.title} directed by ${this.director} lasts for ${this.duration} minutes.`);
+    };
 }
+
+const myMovie = new Movie('Inception', 'Christopher Nolan', 148);
+myMovie.printSummary();  
---
+++ Module 28/urshtrime.js
@@ -0,0 +1,15 @@
+var btn2 = document.getElementById('btn2');
+
+button2.onclick('click',function(){
+    alert('Jona');
+});
+var button3=document.getElementById('btn3')
+
+button3.onclick('click',function(){
+    alert('era');
+});
+var button4 = document.getElementById('btn1');
+
+button4.onclick('click',function(){
+    alert('zana');
+});
