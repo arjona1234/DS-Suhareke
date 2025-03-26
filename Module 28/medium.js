8e239f17cb9ac8e6c107485d20e0cb722aa13475
arjona1234 <arjonaelezi@gmail.com>
2025-03-26 19:35:32 +0100

    .

---
+++ Module 28/medium.js
@@ -0,0 +1,8 @@
+btn1.onclick.function ()
+{
+txt1.style.color="pink";
+txt1.style.backgroundColor="lightgrey";
+txt1.style.textAlign="center";
+txt1.style.fontSize="100px";
+txt1.style.padding="20px";
+}
