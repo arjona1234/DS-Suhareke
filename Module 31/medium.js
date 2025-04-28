c741cbb13cdb37a18ae5073987bdf6e08d98c388
arjona1234 <arjonaelezi@gmail.com>
2025-04-28 19:16:22 +0200

    medium

---
+++ Module 31/medium.js
@@ -0,0 +1,12 @@
+function checkPalindromes(wors){
+    for(var i=0; i<words.length; i++){
+        if(worsd[i] === wors[i].split(''),reverse().join('')){
+            console.log("YES");
+        }else
+        {
+console.log("NO");
+        }
+    }
+}
+
+checkPalindromes(["kimik","level","module"]);
