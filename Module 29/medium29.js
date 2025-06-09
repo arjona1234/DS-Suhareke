0057a11793233f180b1803671cde9dc6365d7097
arjona <arjonaelezi@gmail.com>
2025-06-09 18:40:56 +0200

    medium 29

---
+++ Module 29/medium29.js
@@ -0,0 +1,11 @@
+
+var programingLanguage=[1,2,3,4,5];
+
+programingLanguage.push(6);
+console.log(programingLanguage);
+
+programingLanguage.shift(1);
+console.log(programingLanguage);
+
+programingLanguage.splice(0,2,5);
+console.log(programingLanguage);
