b7f420309cf47d78a5af4a9d1a4d6bf88d6e9e13
arjona <arjonaelezi@gmail.com>
2025-06-09 18:57:30 +0200

    hardchallenge29

---
+++ Module 29/hardchallenge29.js
@@ -0,0 +1,10 @@
+const strings = ["molle","qershi","fiq","kumbull","dredhze","boronice"];
+
+const randomIndex = Math.floor(Math.random()*strings.length );
+
+const [extractedWord = '', anotherWord='', ]=[strings[randomIndex],strings[randomIndex+1]];
+if(extractedWord){
+    console.log("ExtactedWord:", extractedWord);
+}else{
+    console.log("No valid extracted word found.");
+}
